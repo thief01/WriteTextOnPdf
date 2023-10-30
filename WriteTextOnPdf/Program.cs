462cfbbca814f2a7cddfc960db89dc4379a3b432
thief01 <pst12345@wp.pl>
2023-10-30 06:33:00 +0100

    fix for pl letters

--- WriteTextOnPdf/Program.cs
+++ WriteTextOnPdf/Program.cs
@@ -1,42 +1,65 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
-namespace RozmiarStronyPDF
+namespace DodawanieTekstuDoPDF
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string sciezkaDoPlikuPDF = "plik.pdf";
-
-            if (System.IO.File.Exists(sciezkaDoPlikuPDF))
+            System.Text.EncodingProvider ppp = System.Text.CodePagesEncodingProvider.Instance;
+            Encoding.RegisterProvider(ppp);
+            // Test();
+            if (args.Length < 2)
             {
-                using (var sr = new System.IO.FileStream(sciezkaDoPlikuPDF, System.IO.FileMode.Open))
-                {
-                    var pdfReader = new PdfReader(sr);
-                    int numerStrony = 1; // Numer strony, którą chcesz sprawdzić
-
-                    if (numerStrony >= 1 && numerStrony <= pdfReader.NumberOfPages)
-                    {
-                        var pageSize = pdfReader.GetPageSize(numerStrony);
-                        float szerokosc = pageSize.Width;
-                        float wysokosc = pageSize.Height;
-
-                        Console.WriteLine($"Rozmiar strony {numerStrony} w punktach:");
-                        Console.WriteLine($"Szerokość: {szerokosc}pt");
-                        Console.WriteLine($"Wysokość: {wysokosc}pt");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Podano niepoprawny numer strony.");
-                    }
-                }
+                Console.WriteLine("InputPath, OutputPath, TextToWrite, TextAlign");
+                Console.WriteLine("- Text align, 0 - left, 1 center, 2 - right");
+                return;
             }
-            else
+            string sciezkaDoPlikuWejsciowego = args[0];
+            string sciezkaDoPlikuWyjsciowego = args[1];
+
+            if (!File.Exists(sciezkaDoPlikuWejsciowego))
             {
-                Console.WriteLine("Plik PDF nie istnieje.");
+                Console.WriteLine($"Plik {sciezkaDoPlikuWejsciowego} nie istnieje.");
+                return;
             }
+            
+            var sr = new FileStream(sciezkaDoPlikuWejsciowego, FileMode.Open);
+            var fs = new FileStream(sciezkaDoPlikuWyjsciowego, FileMode.Create);
+            
+            var pdfReader = new PdfReader(sr);
+            var pdfStamper = new PdfStamper(pdfReader, fs);
+            var contentByte = pdfStamper.GetOverContent(1); // Wybieramy pierwszą stronę PDF
+
+            // Tworzymy obiekt do renderowania tekstu
+            var cb = pdfStamper.GetOverContent(1);
+            
+            // Tworzymy czcionkę
+            // var baseFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.NOT_EMBEDDED);
+            var baseFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.NOT_EMBEDDED);
+            
+            BaseFont bf = BaseFont.CreateFont("ARIALUNI.TTF", BaseFont.IDENTITY_H, true);
+            //Font f = new iTextSharp.text.Font(bf, 12, Font.NORMAL, BaseColor.BLACK);
+
+            // Ustawiamy czcionkę, kolor i rozmiar tekstu
+            cb.SetColorFill(BaseColor.DARK_GRAY);
+            cb.SetFontAndSize(bf, 12);
+
+            // Tworzymy tekst, który chcemy dodać
+            string tekst = "To jest nowy tekst dodany do istniejącego PDF ĄĘŹŻĆŁŃÓ.";
+
+            // Ustalamy pozycję tekstu na stronie
+            cb.BeginText();
+            cb.ShowTextAligned(Element.ALIGN_LEFT, tekst, 100, 700, 0);
+            cb.EndText();
+
+            pdfStamper.Close();
+            
+            
         }
     }
 }
