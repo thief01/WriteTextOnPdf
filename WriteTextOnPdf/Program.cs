8d957d63ce6d2dcbeec21e12ef88dc0c7a79af74
thief01 <pst12345@wp.pl>
2023-10-30 03:17:08 +0100

    add iTextSharp

--- WriteTextOnPdf/Program.cs
+++ WriteTextOnPdf/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
-Console.WriteLine("Hello, World!");
+namespace RozmiarStronyPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sciezkaDoPlikuPDF = "plik.pdf";
+
+            if (System.IO.File.Exists(sciezkaDoPlikuPDF))
+            {
+                using (var sr = new System.IO.FileStream(sciezkaDoPlikuPDF, System.IO.FileMode.Open))
+                {
+                    var pdfReader = new PdfReader(sr);
+                    int numerStrony = 1; // Numer strony, którą chcesz sprawdzić
+
+                    if (numerStrony >= 1 && numerStrony <= pdfReader.NumberOfPages)
+                    {
+                        var pageSize = pdfReader.GetPageSize(numerStrony);
+                        float szerokosc = pageSize.Width;
+                        float wysokosc = pageSize.Height;
+
+                        Console.WriteLine($"Rozmiar strony {numerStrony} w punktach:");
+                        Console.WriteLine($"Szerokość: {szerokosc}pt");
+                        Console.WriteLine($"Wysokość: {wysokosc}pt");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Podano niepoprawny numer strony.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Plik PDF nie istnieje.");
+            }
+        }
+    }
+}
