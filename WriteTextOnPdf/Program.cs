8e878a123c2185a6ff9fcfbc37ae6f962bec676a
thief01 <pst12345@wp.pl>
2023-10-30 06:39:41 +0100

    refactor

--- WriteTextOnPdf/Program.cs
+++ WriteTextOnPdf/Program.cs
@@ -8,6 +8,21 @@ namespace EasyAddTextToPdf
 {
     class Program
     {
+        private class Args
+        {
+            public string InputPath { get; }
+            public string OutputPath { get; }
+            public string Text { get; }
+            public int TextAlign { get; }
+
+            public Args(string[] args)
+            {
+                InputPath = args[0];
+                OutputPath = args[1];
+                Text = args[2];
+                TextAlign = Convert.ToInt32(args[3]);
+            }
+        }
         static void Main(string[] args)
         {
             EncodingProvider ppp = CodePagesEncodingProvider.Instance;
@@ -19,19 +34,18 @@ namespace EasyAddTextToPdf
                 Console.WriteLine("- Text align, 0 - left, 1 center, 2 - right");
                 return;
             }
-            string inputPath = args[0];
-            string outputPath = args[1];
-            string text = args[2];
-            int textAlign = Convert.ToInt32(args[3]);
 
-            if (!File.Exists(inputPath))
+            Args parsedArgs = new Args(args);
+            
+
+            if (!File.Exists(parsedArgs.InputPath))
             {
-                Console.WriteLine($"File \"{inputPath}\" doesn't exist.");
+                Console.WriteLine($"File \"{parsedArgs.InputPath}\" doesn't exist.");
                 return;
             }
             
-            var sr = new FileStream(inputPath, FileMode.Open);
-            var fs = new FileStream(outputPath, FileMode.Create);
+            var sr = new FileStream(parsedArgs.InputPath, FileMode.Open);
+            var fs = new FileStream(parsedArgs.OutputPath, FileMode.Create);
             
             var pdfReader = new PdfReader(sr);
             var pdfStamper = new PdfStamper(pdfReader, fs);
@@ -43,7 +57,7 @@ namespace EasyAddTextToPdf
             cb.SetColorFill(BaseColor.DARK_GRAY);
             cb.SetFontAndSize(bf, 12);
             cb.BeginText();
-            cb.ShowTextAligned(textAlign, text, 100, 700, 0);
+            cb.ShowTextAligned(parsedArgs.TextAlign, parsedArgs.Text, 100, 700, 0);
             cb.EndText();
 
             pdfStamper.Close();
