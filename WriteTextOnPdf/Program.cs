4c7014b34198b8e8dd5d3c782ed91a6e11ed9f79
thief01 <pst12345@wp.pl>
2023-10-30 06:36:11 +0100

    refactor/renames

--- WriteTextOnPdf/Program.cs
+++ WriteTextOnPdf/Program.cs
@@ -4,57 +4,47 @@ using System.Text;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
-namespace DodawanieTekstuDoPDF
+namespace EasyAddTextToPdf
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Text.EncodingProvider ppp = System.Text.CodePagesEncodingProvider.Instance;
+            EncodingProvider ppp = CodePagesEncodingProvider.Instance;
             Encoding.RegisterProvider(ppp);
-            // Test();
-            if (args.Length < 2)
+
+            if (args.Length < 4)
             {
                 Console.WriteLine("InputPath, OutputPath, TextToWrite, TextAlign");
                 Console.WriteLine("- Text align, 0 - left, 1 center, 2 - right");
                 return;
             }
-            string sciezkaDoPlikuWejsciowego = args[0];
-            string sciezkaDoPlikuWyjsciowego = args[1];
+            string inputPath = args[0];
+            string outputPath = args[1];
+            string text = args[2];
+            int textAlign = Convert.ToInt32(args[3]);
 
-            if (!File.Exists(sciezkaDoPlikuWejsciowego))
+            if (!File.Exists(inputPath))
             {
-                Console.WriteLine($"Plik {sciezkaDoPlikuWejsciowego} nie istnieje.");
+                Console.WriteLine($"File \"{inputPath}\" doesn't exist.");
                 return;
             }
             
-            var sr = new FileStream(sciezkaDoPlikuWejsciowego, FileMode.Open);
-            var fs = new FileStream(sciezkaDoPlikuWyjsciowego, FileMode.Create);
+            var sr = new FileStream(inputPath, FileMode.Open);
+            var fs = new FileStream(outputPath, FileMode.Create);
             
             var pdfReader = new PdfReader(sr);
             var pdfStamper = new PdfStamper(pdfReader, fs);
-            var contentByte = pdfStamper.GetOverContent(1); // Wybieramy pierwszą stronę PDF
+            var contentByte = pdfStamper.GetOverContent(1); 
 
-            // Tworzymy obiekt do renderowania tekstu
             var cb = pdfStamper.GetOverContent(1);
-            
-            // Tworzymy czcionkę
-            // var baseFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.NOT_EMBEDDED);
-            var baseFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.NOT_EMBEDDED);
-            
-            BaseFont bf = BaseFont.CreateFont("ARIALUNI.TTF", BaseFont.IDENTITY_H, true);
-            //Font f = new iTextSharp.text.Font(bf, 12, Font.NORMAL, BaseColor.BLACK);
 
-            // Ustawiamy czcionkę, kolor i rozmiar tekstu
+            BaseFont bf = BaseFont.CreateFont("ARIALUNI.TTF", BaseFont.IDENTITY_H, true);
+
             cb.SetColorFill(BaseColor.DARK_GRAY);
             cb.SetFontAndSize(bf, 12);
-
-            // Tworzymy tekst, który chcemy dodać
-            string tekst = "To jest nowy tekst dodany do istniejącego PDF ĄĘŹŻĆŁŃÓ.";
-
-            // Ustalamy pozycję tekstu na stronie
             cb.BeginText();
-            cb.ShowTextAligned(Element.ALIGN_LEFT, tekst, 100, 700, 0);
+            cb.ShowTextAligned(textAlign, text, 100, 700, 0);
             cb.EndText();
 
             pdfStamper.Close();
